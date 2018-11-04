351f335222c9e4e8f07a4ec3c8c1db6f2d1a33d1
Piyush Bhangale <bhangalepiyush@gmail.com>
2018-11-04 16:42:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { CommandoClient } = require('discord.js-commando')
+const path = require('path');
+const client = new CommandoClient({
+    commandPrefix: 's.',
+    owner: ['365644930556755969','346662217510551552'],
+    disableEveryone: true
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['NSFW', 'NSFW Commands']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+    client.on('ready', () => {
+        console.log('Logged in!');
+        client.user.setActivity('and Watching');
+    });
+
+    client.login('Your Secret Token');
